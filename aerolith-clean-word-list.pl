70d9ed8d8769f472f708334297277643df2e58ad
Mike Thelen <boshvark@gmail.com>
2025-07-13 11:16:35 -0400

    Fix aerolith-clean-word-list.pl not to skip words with '-' difficulty.

--- aerolith-clean-word-list.pl
+++ aerolith-clean-word-list.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -ln
 
 # Print only the word column from lines containing full word info.
-next unless s/^(\d+\t)+//;
+next unless s/^([\d-]+\t)+//;
 my ($word) = (split /\t/)[2];
 next unless $word =~ s/^[^A-Z]*([A-Z]+)[^A-Z]*$/$1/;
 print $word;
